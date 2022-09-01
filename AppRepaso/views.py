3f52b11c9364fcc9b76ab037d789faf56defb671
Diego Izquierdo <izquierdodiegom@gmail.com>
2022-09-01 16:59:50 -0300

    fin clase 20

--- AppRepaso/views.py
+++ AppRepaso/views.py
@@ -5,12 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def curso(request):
-    curso=Curso(nombre="AWS", comision=3)
-    curso2=Curso(nombre="Lengua", comision=1)
-    curso3=Curso(nombre="Matematica", comision=2)
+    curso=Curso(nombre="curso creado en el ejemplo 0", comision=3)
+    print("creando curso")
     curso.save()
-    curso2.save()
-    curso3.save()
+    
     texto=f"Cursos creados"
     return HttpResponse(texto)
 
