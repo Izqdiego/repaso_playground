808fcbd3816f0b682a0a0ce4341e1fd571427627
Diego Izquierdo <izquierdodiegom@gmail.com>
2022-09-03 20:50:10 -0300

    busqueda en BD

--- AppRepaso/urls.py
+++ AppRepaso/urls.py
@@ -1,12 +1,17 @@
+from msilib.schema import Patch
 from django.urls import path
 from AppRepaso.views import *
 
 
 urlpatterns = [
     path('curso/', curso),
-    path('inicio', inicio, name="inicio"),
+    path('', inicio, name="inicio"),
     path('cursos/', cursos, name="cursos"),
     path('estudiantes/', estudiantes, name="estudiantes"),
     path('profesores/', profesores, name="profesores"),
     path('entregables/', entregables, name="entregables"),
+    path('curso/', curso, name="curso"),
+    path('formularioProfe/', formularioProfe, name="formularioProfe"),
+    path('busquedaComision/', busquedaComision, name="busquedacomision"),
+    path('buscar/', buscar, name="buscar")
 ]
--- AppRepaso/views.py
+++ AppRepaso/views.py
@@ -1,6 +1,9 @@
 from http.client import HTTPResponse
+from urllib.request import Request
 from django.shortcuts import render
-from .models import Curso
+
+from AppRepaso.forms import CursoForm, ProfeForm
+from .models import Curso, Profesor
 from django.http import HttpResponse
 # Create your views here.
 
@@ -27,3 +30,59 @@ def profesores(request):
 def entregables(request):
     return render(request, "AppRepaso/entregables.html")
 
+"""def cursoFormulario(request):
+    if request.method=="POST":
+        nombre=request.POST["nombre"]
+        comision=request.POST["comision"]
+        curso=Curso(nombre=nombre, comision=comision)
+        curso.save()
+        return render(request, "AppRepaso/inicio.html")
+    return render(request, "AppRepaso/cursoFormulario.html")"""
+    
+def curso(request):
+    if request.method=="POST":
+        formu=CursoForm(request.POST)
+        print("---------------------")
+        print(formu)
+        print("---------------------")
+        if formu.is_valid():
+            informacion=formu.cleaned_data
+            print(informacion)
+            nombre=informacion["nombre"]
+            comision=informacion["comision"]
+            curso=Curso(nombre=nombre, comision=comision)
+            curso.save()
+            return render(request, "AppRepaso/inicio.html")    
+    else:
+        formulario=CursoForm()
+        return render (request, "AppRepaso/curso.html", {"formulario":formulario})
+
+def formularioProfe(request):
+
+
+    if request.method=="POST":
+        form=ProfeForm(request.POST)     
+        if form.is_valid():
+            info=form.cleaned_data
+            nombre=info["nombre"]
+            apellido=info["apellido"]
+            email=info["email"]
+            profesion=info["profesion"]
+            profe=Profesor(nombre=nombre, apellido=apellido, email=email, profesion=profesion)
+            profe.save()
+            return render (request,"AppRepaso/inicio.html")
+    else:
+        formulario=ProfeForm()
+        return render(request, "AppRepaso/formularioProfe.html", {"formulario":formulario})
+    
+def busquedaComision(request):
+    return render(request, "AppRepaso/busquedaComision.html")
+
+def buscar(request):
+    if request.GET["comision"]:
+        comision=request.GET["comision"]
+        # trae de la base de datos: del modelo "Curso" de los "objetos" filtrar los que comision sea igual a la comision que estamso buscando 
+        cursos=Curso.objects.filter(comision=comision)
+        return render(request, "AppRepaso/resultadosBusqueda.html", {"cursos":cursos})
+    else:
+        return render(request, "AppRepaso/busquedaComision.html", {"mensaje":"Ingrese una comision"})
